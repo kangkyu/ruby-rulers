ebca8bef20a675ab7e964d7efb0011a1b98432a9
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-07-15 23:02:55 -0700

    Chapter 4: Use erubis, pull out controller as separate file

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,6 +2,7 @@ require 'rulers/version'
 require 'rulers/routing'
 require 'rulers/util'
 require 'rulers/dependencies'
+require 'rulers/controller'
 
 module Rulers
   class Application
@@ -10,16 +11,6 @@ module Rulers
       controller = klass.new(env)
       text = controller.send(act)
       [200, {'Content-Type' => 'text/html'}, [text]]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
     end
   end
 end
