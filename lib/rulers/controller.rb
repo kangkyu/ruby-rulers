3a4bbfed1e1062f7fa49a60b057d990d704f502b
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-07-16 19:40:56 -0700

    Chapter 6: use Rack::Request#params method

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -1,5 +1,6 @@
 require 'erubis'
 require 'rulers/file_model'
+require 'rack/request'
 
 module Rulers
   class Controller
@@ -24,6 +25,14 @@ module Rulers
       klass = self.class
       klass = klass.to_s.gsub /Controller$/, ""
       Rulers.to_underscore klass
+    end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params
     end
   end
 end
